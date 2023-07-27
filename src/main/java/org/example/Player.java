34562c4d60d07c77b7b89457cb780c6adf9e231a
Руслан Кальчевский <rusya754@gmail.com>
2023-07-27 12:41:02 +0600

    частичная реализация игрового поля_2

---
+++ src/main/java/org/example/Player.java
@@ -0,0 +1,15 @@
+package org.example;
+
+abstract class Player {
+    protected Disk colorOfPlayersDisk; //заменил с чара на диск
+
+    public Player(Disk disk) {
+        this.colorOfPlayersDisk = disk;
+    }
+
+    public abstract int[] makeMove();
+
+    public Disk getOpponent() {
+        return (colorOfPlayersDisk == Disk.BLACK) ? Disk.WHITE : Disk.BLACK;
+    }
+}
